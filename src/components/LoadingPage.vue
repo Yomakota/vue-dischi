172171df8a23fd262acd283e0a4ad3ec07db91be
Yomakota <89361386+Yomakota@users.noreply.github.com>
2022-07-09 18:03:48 +0200

    add bg-color loader

--- src/components/LoadingPage.vue
+++ src/components/LoadingPage.vue
@@ -1,24 +1,37 @@
 <template>
-        <div class="loader-container">
-            <div class="loader position-absolute top-50 start-50 translate-middle">
-                <div class="hourglass pb-3"></div>
-                <div class="text-loader text-uppercase">Loading...</div>
-            </div>
+    <div class="loader-container">
+        <div class="loader position-absolute top-50 start-50 translate-middle">
+            <div class="hourglass pb-3"></div>
+            <div class="text-uppercase">Loading...</div>
         </div>
+    </div>
 </template>
 <script>
 export default {
     name: 'LoadingPage',
-    data(){
-        return{
+    data() {
+        return {
         }
     }
 }
 </script>
 <style lang="scss" scoped>
-@import'../assets/scss/loader.scss';
+@import'../assets/scss/style.scss';
 
-.loader{
-    color: white;
+.loader-container {
+    position: fixed;
+    top: 0;
+    left: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    background-color: white;
+
+    .loader {
+        color: rgb(10, 10, 10);
+    }
 }
 </style>
